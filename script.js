59e85d57b830b9b6dba1d37a17b3c57683292827
jabezdubale <jabezdubale6@gmail.com>
2025-06-17 16:41:58 +0300

    0. Form Creation and Validation

--- script.js
+++ script.js
@@ -2,10 +2,42 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
-  form.document.addEventListener("submit", (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
+
+    let isValid = true;
+    let messages = [];
+
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Name length is less than 3");
+    }
+
+    if (!email.includes("@")) {
+      isValid = false;
+      messages.push("Email does not contain @");
+    } else if (!email.includes(".")) {
+      isValid = false;
+      messages.push("Email does not contain .");
+    }
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password is too short");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+      form.submit();
+    } else {
+      feedbackDiv.style.color = "#dc3545";
+      feedbackDiv.innerHTML = messages.map((message) => {
+        return `${message}<br>`;
+      });
+    }
   });
 });
